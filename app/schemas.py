c7f017e0a452b0aafa2c77daee43770abb120cf2
Mihaly Nyilas <dr.dabzse@gmail.com>
2025-01-29 20:10:04 +0100

    update + add: models and schemas

---
+++ app/schemas.py
@@ -0,0 +1,37 @@
+from pydantic import BaseModel
+
+
+class NoteBase(BaseModel):
+    title: str
+    content: str
+
+
+class NoteCreate(NoteBase):
+    pass
+
+
+class NoteUpdate(NoteBase):
+    pass
+
+
+class NoteInDB(NoteBase):
+    id: int
+    owner_id: int
+
+    class Config:
+        from_attributes = True
+
+
+class UserBase(BaseModel):
+    username: str
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class UserInDB(UserBase):
+    id: int
+
+    class Config:
+        from_attributes = True
