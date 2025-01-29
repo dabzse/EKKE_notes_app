5d4c6e6384245a6b4fe0f6f93228aaf9a8a5dd80
Mihaly Nyilas <dr.dabzse@gmail.com>
2025-01-29 21:45:03 +0100

    add: http://localhost:8000/ default page (multiform)

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from .auth.auth_routes import router as auth_router
@@ -6,8 +6,13 @@ from .notes.notes_routes import router as notes_router
 
 app = FastAPI()
 
-# app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+templates = Jinja2Templates(directory="app/templates")
 
 app.include_router(auth_router, prefix="/auth", tags=["Auth"])
 app.include_router(notes_router, prefix="/notes", tags=["Notes"])
+
+
+@app.get("/")
+async def index(request: Request):
+    return templates.TemplateResponse("multiform.html", {"request": request})
