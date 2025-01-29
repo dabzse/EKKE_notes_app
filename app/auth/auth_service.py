fb40050dd72fecdfe55aead883a41c7f18479b74
Mihaly Nyilas <dr.dabzse@gmail.com>
2025-01-29 20:40:21 +0100

    add: generated secret key

--- app/auth/auth_service.py
+++ app/auth/auth_service.py
@@ -1,8 +1,13 @@
+from datetime import datetime, timedelta, timezone
+
 from jose import JWTError, jwt
 from passlib.context import CryptContext
-from datetime import datetime, timedelta
 
-SECRET_KEY = "your-secret-key"
+"""
+to get a SECRET_KEY string, run:
+openssl rand -hex 32
+"""
+SECRET_KEY = "222adc0c77257f628b56c5502b6b2e2b5018d95cb7efbc1ebb50657f26faac49"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
@@ -11,14 +16,17 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 def hash_password(password: str):
     return pwd_context.hash(password)
 
+
 def verify_password(plain_password: str, hashed_password: str):
     return pwd_context.verify(plain_password, hashed_password)
 
+
 def create_access_token(data: dict):
     to_encode = data.copy()
-    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
 
 def decode_access_token(token: str):
     try:
