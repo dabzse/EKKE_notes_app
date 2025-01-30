a19ed596e68b9bb24b1eeec319a66c2199343b3a
Mihaly Nyilas <dr.dabzse@gmail.com>
2025-01-30 16:27:03 +0100

    now works

--- app/auth/auth_service.py
+++ app/auth/auth_service.py
@@ -21,16 +21,21 @@ def verify_password(plain_password: str, hashed_password: str):
     return pwd_context.verify(plain_password, hashed_password)
 
 
-def create_access_token(data: dict):
-    to_encode = data.copy()
+def create_access_token(payload: dict):
+    if not isinstance(payload, dict):
+        raise ValueError("payload must be a dictionary")
     expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    to_encode.update({"exp": expire})
-    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    payload.update({"exp": expire})
+    return jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
 
 
 def decode_access_token(token: str):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        sub = payload.get('sub')
+        if not sub:
+            raise ValueError("Invalid 'sub' field in token payload")
         return payload
-    except JWTError:
+    except (JWTError, ValueError) as e:
+        print(f"Error decoding token: {e}")
         return None
