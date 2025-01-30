9d8356caa5b355c734e4ac473184175d8ea31659
Mihaly Nyilas <dr.dabzse@gmail.com>
2025-01-30 16:22:26 +0100

    .env pathonpath import corrected, notes service removed + add init_db

--- app/main.py
+++ app/main.py
@@ -2,9 +2,10 @@ from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from .auth.auth_routes import router as auth_router
-from .notes.notes_routes import router as notes_router
-from .notes.notes_service import router as notes_service_router
+
+from app.database import init_db
+from app.auth.auth_routes import router as auth_router
+from app.notes.notes_routes import router as notes_router
 
 app = FastAPI(
     title="FastAPI Course :: EKKE: LBT_IM738G2",
@@ -21,6 +22,8 @@ app = FastAPI(
     },
 )
 
+init_db()
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -35,8 +38,6 @@ templates = Jinja2Templates(directory="app/templates")
 
 app.include_router(auth_router, prefix="/auth", tags=["Auth"])
 app.include_router(notes_router, prefix="/notes", tags=["Notes"])
-app.include_router(notes_service_router, prefix="/notes_service", tags=["Notes Service"])
-
 
 
 @app.get("/")
