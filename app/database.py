ef95b3e89fb6f8e40671d1da784bfb57aba528b7
Mihaly Nyilas <dr.dabzse@gmail.com>
2025-01-30 16:21:03 +0100

    small changes

--- app/database.py
+++ app/database.py
@@ -1,11 +1,13 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./simple_notes.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./simple_notes.db"
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
 
 def get_db():
     db = SessionLocal()
@@ -13,3 +15,8 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+def init_db():
+    import app.models
+    Base.metadata.create_all(bind=engine)
