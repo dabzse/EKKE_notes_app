fce623a01187ad82224dd2d92315f9d1f8e3add4
Mihaly Nyilas <dr.dabzse@gmail.com>
2025-01-30 16:23:07 +0100

    crud helper for notes

--- app/crud.py
+++ app/crud.py
@@ -1,25 +1,25 @@
 from sqlalchemy.orm import Session
-from .models import Note
+from app.models import Note
 
 
-def get_note_by_id(db: Session, note_id: int, user_id: int):
-    return db.query(Note).filter(Note.id == note_id, Note.owner_id == user_id).first()
-
-
-def create_note(db: Session, title: str, content: str, user_id: int):
-    note = Note(title=title, content=content, owner_id=user_id)
+def create_note(db: Session, title: str, content: str, owner: str):
+    note = Note(title=title, content=content, owner=owner)
     db.add(note)
     db.commit()
     db.refresh(note)
     return note
 
 
-def get_notes(db: Session, user_id: int):
-    return db.query(Note).filter(Note.owner_id == user_id).all()
+def get_notes(db: Session, owner: str):
+    return db.query(Note).filter(Note.owner == owner).all()
 
 
-def update_note(db: Session, note_id: int, title: str, content: str, user_id: int):
-    note = db.query(Note).filter(Note.id == note_id, Note.owner_id == user_id).first()
+def get_note_by_id(db: Session, note_id: int, owner: str):
+    return db.query(Note).filter(Note.id == note_id, Note.owner == owner).first()
+
+
+def update_note(db: Session, note_id: int, title: str, content: str, owner: str):
+    note = db.query(Note).filter(Note.id == note_id, Note.owner == owner).first()
     if note:
         note.title = title
         note.content = content
@@ -28,8 +28,8 @@ def update_note(db: Session, note_id: int, title: str, content: str, user_id: in
     return note
 
 
-def delete_note(db: Session, note_id: int, user_id: int):
-    note = db.query(Note).filter(Note.id == note_id, Note.owner_id == user_id).first()
+def delete_note(db: Session, note_id: int, owner: str):
+    note = db.query(Note).filter(Note.id == note_id, Note.owner == owner).first()
     if note:
         db.delete(note)
         db.commit()
